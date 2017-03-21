bd41b1c52693071c356a5850ea35c7a4b245bf81
William Blankenship <william.jblankenship@gmail.com>
2017-03-21 12:19:29 -0400

    Add template w/ install script
    Install handles all the logic for naming your application, inserting it
    into $PATH, adding it as an exception in the .gitignore file, etc.

---
+++ template/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "${SOURCE}" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "${DIR}"
+IMAGE_NAME="$(basename ${DIR})"
+mv cmd "${IMAGE_NAME}"
+cd ../.bin
+ln -s "${DIR}/${IMAGE_NAME}" .
+echo "\!$(basename ${DIR})/${IMAGE_NAME}" >> ../.gitignore
+rm "${DIR}/install.sh"
